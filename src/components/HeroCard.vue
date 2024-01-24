9450c05f4b2c86cbddeda032f7c708fec17157f7
Sam Macharia <smm_mwangii@live.com>
2024-01-24 14:52:12 +0000

    updated positioning of hero name

--- src/components/HeroCard.vue
+++ src/components/HeroCard.vue
@@ -13,10 +13,11 @@ defineProps<{ character: Character }>()
     <img
       :src="checkImagePath(character)"
       :alt="character.name"
+      lazy
       class="shadow-2xl object-cover h-72 w-full overflow-hidden"
     />
     <span
-      class="hidden absolute top-28 left-40 text-xl bg-red-600 text-white p-2 -skew-x-6 group-hover:block transition-all"
+      class="hidden absolute top-28 right-0 text-xl bg-red-600 text-white p-2 -skew-x-6 group-hover:block transition-all"
     >
       {{ character.name }}
     </span>
